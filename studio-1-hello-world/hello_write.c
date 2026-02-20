54cd38c3a239b973157c19759181b3583439bd22
Chetan K. Uppara <cuppara@h5.mcs.slu.edu>
2026-02-19 22:48:18 -0600

    Replaced template files with completed code

--- studio-1-hello-world/hello_write.c
+++ studio-1-hello-world/hello_write.c
@@ -1,1 +1,12 @@
-//Replace the contents of this file with your hello_write.c
+// Chetan Uppara
+// 02/19/2026
+// Hello World using write system call
+
+#include <unistd.h>
+
+int main(int argc, char* argv[]) {
+    const char message[] = "Hello, world!\n";
+    write(STDOUT_FILENO, message, 1000);
+    return 0;
+}
+
